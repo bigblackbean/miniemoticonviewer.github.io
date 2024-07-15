a5065f688845e81178ca843cbf94bdf030daede4
bigblackbean <bigblackbean@naver.com>
2024-07-15 15:17:25 +0900

    세팅

---
+++ src/app/viewer.tsx
@@ -0,0 +1,7 @@
+"use client";
+import { FunctionComponent } from "react";
+
+const Viewer: FunctionComponent = () => {
+  return <div className="bg-black text-white">viewer</div>;
+};
+export default Viewer;
